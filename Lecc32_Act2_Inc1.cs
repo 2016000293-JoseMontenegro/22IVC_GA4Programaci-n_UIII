fc8fcf7f31d24c0696e6ad2019e84b025e9b1757
2016000293-JoseMontenegro <j.montenegro5adb@gmail.com>
2026-07-15 09:05:06 -0600

    Add files via upload

---
+++ Lecc32_Act2_Inc1.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: José Montenegro \n" +
+           "Grado: IVC - Clave = 22\n" +
+           "===CALCULADORA DE CAMBIO===");
+        int pago;
+        string producto;
+        Console.WriteLine("\nEscriba el nombre del producto: ");
+        producto = Console.ReadLine();
+        Console.Write("Dame el precio del producto: ");
+        int precio = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite el valor del billete: ");
+        pago = Convert.ToInt32(Console.ReadLine());
+        int cambio = pago - precio;
+        Console.WriteLine("Su cambio es: " + cambio);
+
+    }
+}
